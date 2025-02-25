8eaa5590d3794623d477aed34b1185d206e82874
cindygnaedinger <100149373+cindygnaedinger@users.noreply.github.com>
2025-02-25 14:10:31 -0300

    Agrega funcionalidades a appwrite.js

--- src/appwrite.js
+++ src/appwrite.js
@@ -1,8 +1,38 @@
+import { Client, Databases, Query } from "appwrite";
+
 const DATABASE_ID = import.meta.env.VITE_APPWRITE_DATABASE_ID;
 const COLLECTION_ID = import.meta.env.VITE_APPWRITE_COLLECTION_ID;
 const PROJECT_ID = import.meta.env.VITE_APPWRITE_PROJECT_ID;
 
-export const updateSearchCount = async () => {
-    console.log(PROJECT_ID, COLLECTION_ID, DATABASE_ID);
+const client = new Client()
+    .setEndpoint("https://cloud.appwrite.io/v1")
+    .setProject(PROJECT_ID)
+
+const database = new Databases(client);
+
+export const updateSearchCount = async (searchTerm, movie) => {
+    // 1. uso el sdk de appwrite para verificar si el término se buscó antes
+try {
+    const result = await database.listDocuments(DATABASE_ID, COLLECTION_ID, [ Query.equal("searchTerm", searchTerm),])
+    // 2. si existe, actualizar el contador
+    if(result.documents.length > 0) {
+        const doc = result.documents[0];
+
+        await database.updateDocument(DATABASE_ID, COLLECTION_ID, doc.$id, {count: doc.count + 1,
+        })
+    // 3. si no existe, crear un nuevo documento con el término buscado y el contador en 1
+    } else {
+        await database.createDocument(DATABASE_ID, COLLECTION_ID, ID.unique(), {
+            searchTerm,
+            count: 1,
+            movie_id: movie.id,
+            poster_url: `https://image.tmdb.org/t/p/w500${movie.poster_pat}`,
+        })
+    }
+} catch (error) {
+    console.log(error);
+}
+    
+
 
 };
